d76c85c39a69eb3648fb9d35446425a5068f5255
van-gomes <nninha.gomes@gmail.com>
2025-09-27 18:27:16 -0300

    make setup tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
 
-// https://vite.dev/config/
+import react from "@vitejs/plugin-react";
+import type { UserConfig } from "vite";
+import { defineConfig } from "vite";
+import type { InlineConfig } from "vitest";
+import tailwindcss from "@tailwindcss/vite";
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true,
+    setupFiles: ["./test/setup.ts"],
+    environment: "happy-dom",
+  },
+} as UserConfig & {
+  test: InlineConfig;
+});
