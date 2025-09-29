bfacac0ba642c982523d768bfbc8d991b8f5a700
van-gomes <nninha.gomes@gmail.com>
2025-09-29 17:01:05 -0300

    setup shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,13 @@
-import path from "path";
-
-import react from "@vitejs/plugin-react";
-import type { UserConfig } from "vite";
+import path from "node:path";
 import { defineConfig } from "vite";
-import type { InlineConfig } from "vitest";
-import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  test: {
-    globals: true,
-    setupFiles: ["./test/setup.ts"],
-    environment: "happy-dom",
-  },
-} as UserConfig & {
-  test: InlineConfig;
 });
