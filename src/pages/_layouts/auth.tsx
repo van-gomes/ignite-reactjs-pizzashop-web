6f0f33bf95fa5638c8b8be0cf2d0840faebc23df
van-gomes <nninha.gomes@gmail.com>
2025-10-10 16:46:35 -0300

    feat: continue making login page

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,4 +1,5 @@
 
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
@@ -6,7 +7,7 @@ export function AuthLayout() {
     <div className="grid min-h-screen grid-cols-2">
       <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         <div className="flex items-center gap-3 text-lg text-foreground">
-          {/* <Pizza className="h-5 w-5" /> */}
+          <Pizza className="h-5 w-5" />
           <span className="font-semibold">pizza.shop</span>
         </div>
 
