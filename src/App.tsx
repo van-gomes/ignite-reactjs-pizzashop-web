74e4ea57829b990b7b05e813917ecf2481d50dba
van-gomes <nninha.gomes@gmail.com>
2025-10-14 18:20:23 -0300

    feat: ThemeToggle and Account Menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './theme/theme-provider'
 import './global.css'
 
 export function App() {
-    return (
+     return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
 
-      <Toaster richColors />
+        <Toaster richColors />
 
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,8 @@ import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
 import { NavLink } from '../components/nav-link'
 import { Separator } from '../ui/separator'
+import { AccountMenu } from './account-menu'
+import { ThemeToggle } from '../theme/theme-toggle'
 
 export function Header() {
   return (
@@ -21,6 +23,11 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
       </div>
     </div>
   )
